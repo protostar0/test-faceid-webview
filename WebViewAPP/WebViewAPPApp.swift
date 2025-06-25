4fb574c6a01350a5ad014b83c78a95572f388cee
Abdelhak <48460845+protostar0@users.noreply.github.com>
2025-06-25 16:48:04 +0400

    Initial Commit

---
+++ WebViewAPP/WebViewAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewAPPApp.swift
+//  WebViewAPP
+//
+//  Created by Abdelhak Kherroubi on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WebViewAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebViewAPPTests/WebViewAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewAPPTests.swift
+//  WebViewAPPTests
+//
+//  Created by Abdelhak Kherroubi on 25/06/2025.
+//
+
+import Testing
+@testable import WebViewAPP
+
+struct WebViewAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
